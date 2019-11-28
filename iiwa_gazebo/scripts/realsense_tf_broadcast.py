20d674922f3244f84c47fa3ad33a29f9e56547e6
linzhank <linzhank@gmail.com>
2019-11-28 12:14:31 -0500

    test tf broadcast for camera

---
+++ iiwa_gazebo/scripts/realsense_tf_broadcast.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import roslib
+roslib.load_manifest('iiwa_gazebo')
+
+import rospy
+import tf
+from math import pi
+
+YAW, PITCH, ROLL = -pi/2, 0.010, -0.012
+ROT = tf.transformations.quaternion_from_euler(YAW, PITCH, ROLL, 'rzyx')
+TRANS = (-0.03, 0.0175, 0.03880)
+if __name__ == '__main__':
+    rospy.init_node('realsense_tf_broadcaster')
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(30.0)
+    while not rospy.is_shutdown():
+        br.sendTransform(TRANS,
+                         ROT,
+                         rospy.Time.now(),
+                         "realsense_d435",
+                         "iiwa_link_ee")
+        rate.sleep()
