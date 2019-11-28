1852bbdb754d7740091bb0a69de4a8cff4405f8e
linzhank <linzhank@gmail.com>
2019-11-28 14:37:29 -0500

    define another tf 'rs_D435' to 'iiwa_link_0'

--- iiwa_gazebo/scripts/realsense_tf_broadcast.py
+++ iiwa_gazebo/scripts/realsense_tf_broadcast.py
@@ -6,9 +6,15 @@ import rospy
 import tf
 from math import pi
 
-YAW, PITCH, ROLL = -pi/2, 0.010, -0.012
+# child of iiwa_link_0
+YAW, PITCH, ROLL = -pi/2, 0.00, -pi/2
 ROT = tf.transformations.quaternion_from_euler(YAW, PITCH, ROLL, 'rzyx')
-TRANS = (-0.03, 0.0175, 0.03880)
+TRANS = (0.206, 0., 0.168)
+
+# child of iiwa_link_ee
+# YAW, PITCH, ROLL = -pi/2, 0.010, -0.012
+# ROT = tf.transformations.quaternion_from_euler(YAW, PITCH, ROLL, 'rzyx')
+# TRANS = (-0.03, 0.0175, 0.03880)
 if __name__ == '__main__':
     rospy.init_node('realsense_tf_broadcaster')
     br = tf.TransformBroadcaster()
@@ -17,6 +23,6 @@ if __name__ == '__main__':
         br.sendTransform(TRANS,
                          ROT,
                          rospy.Time.now(),
-                         "realsense_d435",
-                         "iiwa_link_ee")
+                         "rs_d435",
+                         "iiwa_link_0")
         rate.sleep()
