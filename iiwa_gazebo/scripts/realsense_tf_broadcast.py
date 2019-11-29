9cab1bf2737051d63a43f3e93a4c0d1e5833aaec
linzhank <linzhank@gmail.com>
2019-11-29 16:17:52 -0500

    refine transformation

--- iiwa_gazebo/scripts/realsense_tf_broadcast.py
+++ iiwa_gazebo/scripts/realsense_tf_broadcast.py
@@ -7,9 +7,9 @@ import tf
 from math import pi
 
 # child of iiwa_link_0
-YAW, PITCH, ROLL = -pi/2, 0.00, -pi/2
+YAW, PITCH, ROLL = -pi/2, 0.00, -pi/2+0.1
 ROT = tf.transformations.quaternion_from_euler(YAW, PITCH, ROLL, 'rzyx')
-TRANS = (0.206, 0., 0.168)
+TRANS = (0.204, 0., 0.175)
 
 # child of iiwa_link_ee
 # YAW, PITCH, ROLL = -pi/2, 0.010, -0.012
